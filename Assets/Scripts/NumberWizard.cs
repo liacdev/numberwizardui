dc917f1e5680f3310a9410b02375b5aa225247f9
liacdev <jagoblacksky@gmail.com>
2022-07-13 16:33:05 -0500

    Random Range added

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberWizard : MonoBehaviour
 {
         [SerializeField] int max;
         [SerializeField] int min;
-        
+        [SerializeField] TextMeshProUGUI guessText;
+
         int guess;
 
     void Start()
@@ -40,9 +42,8 @@ public class NumberWizard : MonoBehaviour
     
     void StartGame()
     {
-        max = max + 1;
-        guess = (max + min) / 2;
-
+        // guess = (max + min) / 2;
+        NextGuess();
 
         // Debug.Log("You play Number Wizard now!");
         // Debug.Log("Pick number, don't tell number...");
@@ -54,21 +55,20 @@ public class NumberWizard : MonoBehaviour
        
     public void OnPressHigher()
     {
-            min = guess;
+            min = guess + 1;
             NextGuess();
     }
 
     public void OnPressLower()
     {
-            max = guess;
+            max = guess - 1;
             NextGuess();
     }
 
     void NextGuess()
     {
-            guess = (max + min) / 2;
+            guess = Random.Range(min, max + 1);
+            guessText.text = guess.ToString();
             // Debug.Log("You number more big or more small am: " + guess);
     }
-
-
 }
